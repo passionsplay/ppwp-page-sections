5f970da879ffd0cbf84743ec2d4869006751f0af
Benjamin Turner <benjamin@passionsplay.com>
2016-10-18 12:52:02 -0700

    Define WordPress Plugin meta

---
+++ ppwp-page-sections.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The Passions Play Page Section plugin bootstrap file
+ *
+ * This file is read by WordPress to generate the plugin information in the plugin
+ * admin area. This file also includes all of the dependencies used by the plugin,
+ * registers the activation and deactivation functions, and defines a function
+ * that starts the plugin.
+ *
+ * @link              http://passionsplay.com
+ * @since             0.0.1
+ * @package           PPWP_Page_Sections
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Passions Play Page Section
+ * Description:       Create and organize page sections.
+ * Version:           0.0.1
+ * Author:            Benjamin Turner
+ * Author URI:        http://passionsplay.com
+ * Plugin URI:        http://passionsplay.com/plugins/ppwp-page-sections
+ * Text Domain:       ppwp-page-sections
+ * Domain Path:       /languages
+ */
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+    die;
+}
+
