2f8c359464f2bdaf4d357fd1eb997f6e0d5f03aa
Benjamin Turner <benjamin@passionsplay.com>
2016-10-26 15:44:25 -0700

    Render the page_section shortcode

--- src/Shortcodes/PageSection.php
+++ src/Shortcodes/PageSection.php
@@ -1,4 +1,6 @@
 <?php namespace PassionsPlay\PageSections\Shortcodes;
+
+use PassionsPlay\PageSections\PPWPPageSection;
 /**
  * Defines the shortcodes relating to page_section custom post type.
  *
@@ -11,7 +13,18 @@
 class PageSection {
 
 	public function page_section($atts) {
-		return '';
+		$a = shortcode_atts( array(
+			'slug' => '',
+		), $atts );
+		$page_section_post = PPWPPageSection::get_from_slug($a['slug']);
+		ob_start();
+		if ( $page_section_post ) :
+			$page_section = new PPWPPageSection($page_section_post); ?>
+			<section id="<?php echo $page_section->get_el_id(); ?>" class="<?php echo $page_section->get_el_classes(); ?>">
+				<?php echo $page_section->get_content(); ?>
+			</section>
+		<?php endif;
+		return ob_get_clean();
 	}
 
 }
